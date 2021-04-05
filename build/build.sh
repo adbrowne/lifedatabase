fea3889fce5fec1ae21f8f0f7932c463dc85b3de
Andrew Browne <abrowne@seek.com.au>
2021-04-05 18:48:38 +1000

    Build and bush docker container

--- build/build.sh
+++ build/build.sh
@@ -2,5 +2,12 @@
 echo Entered the build phase...
 echo Build started on `date`
 nvm use lts
+cd api
+$(aws ecr get-login --region us-west-2)
+docker build . -t 662430452979.dkr.ecr.us-west2.amazonaws.com/lifedatabase-api
+docker push 662430452979.dkr.ecr.us-west-2.amazonaws.com/lifedatabase-api
+
+cd ..
+
 terraform init
 terraform apply -auto-approve
