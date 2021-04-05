207878c8af713436470a601dd35d7d7f5136000e
Andrew Browne <abrowne@seek.com.au>
2021-04-05 19:07:38 +1000

    Build: Create ECR repository

--- build/build.sh
+++ build/build.sh
@@ -7,6 +7,10 @@ echo Build started on `date`
 #nvm use lts
 cd api
 aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin 662430452979.dkr.ecr.us-west-2.amazonaws.com
+
+REPO_NAME=lifedatabase-api
+aws ecr describe-repositories --repository-names ${REPO_NAME} --region us-west-2 || aws ecr create-repository --repository-name ${REPO_NAME} --region us-west-2
+
 docker build . -t 662430452979.dkr.ecr.us-west-2.amazonaws.com/lifedatabase-api
 docker push 662430452979.dkr.ecr.us-west-2.amazonaws.com/lifedatabase-api:latest
 
