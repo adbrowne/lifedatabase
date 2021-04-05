b8c3548fdf455263facc4e960a74e4c182e0e0ee
Andrew Browne <abrowne@seek.com.au>
2021-04-05 18:52:14 +1000

    Build: push :latest tag to ecr

--- build/build.sh
+++ build/build.sh
@@ -5,7 +5,7 @@ nvm use lts
 cd api
 $(aws ecr get-login --region us-west-2)
 docker build . -t 662430452979.dkr.ecr.us-west2.amazonaws.com/lifedatabase-api
-docker push 662430452979.dkr.ecr.us-west-2.amazonaws.com/lifedatabase-api
+docker push 662430452979.dkr.ecr.us-west2.amazonaws.com/lifedatabase-api:latest
 
 cd ..
 
