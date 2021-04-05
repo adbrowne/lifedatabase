708f6e41e043ff26b7d18f64e8063fa3bb49a852
Andrew Browne <abrowne@seek.com.au>
2021-04-05 18:59:06 +1000

    Build: Fix ecr uri

--- build/build.sh
+++ build/build.sh
@@ -4,8 +4,8 @@ echo Build started on `date`
 nvm use lts
 cd api
 aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin 662430452979.dkr.ecr.us-west-2.amazonaws.com
-docker build . -t 662430452979.dkr.ecr.us-west2.amazonaws.com/lifedatabase-api
-docker push 662430452979.dkr.ecr.us-west2.amazonaws.com/lifedatabase-api:latest
+docker build . -t 662430452979.dkr.ecr.us-west-2.amazonaws.com/lifedatabase-api
+docker push 662430452979.dkr.ecr.us-west-2.amazonaws.com/lifedatabase-api:latest
 
 cd ..
 
