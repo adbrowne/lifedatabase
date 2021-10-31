67ca0ca39b4bcd619f5f8f0d7455739b5127336b
Andrew Browne <abrowne@seek.com.au>
2021-11-01 10:49:32 +1100

    Integrate netlify identity

--- netlify/pages/index.tsx
+++ netlify/pages/index.tsx
@@ -2,8 +2,24 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import { useEffect, useState } from 'react'
+import netlifyAuth from '../components/netlifyAuth.js'
 
 const Home: NextPage = () => {
+  let [loggedIn, setLoggedIn] = useState(netlifyAuth.isAuthenticated)
+
+  useEffect(() => {
+    netlifyAuth.initialize((user : any) => {
+      setLoggedIn(!!user)
+    })
+  }, [loggedIn])
+
+  let login = () => {
+    netlifyAuth.authenticate((user : any) => {
+      setLoggedIn(!!user)
+    })
+  }
+
   return (
     <div className={styles.container}>
       <Head>
@@ -11,6 +27,14 @@ const Home: NextPage = () => {
         <meta name="description" content="For all your life's data" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
+      {loggedIn ? (
+          <div>
+            You&apos;re logged in!
+          </div>
+        ) : (
+          <button onClick={login}>Log in</button>
+        )}
 
       <main className={styles.main}>
         <h1 className={styles.title}>
